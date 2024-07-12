c3b5ee863355d53febe0875ead15e43538631d51
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-07-12 11:34:02 +0200

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,34 +2,40 @@
 """Authenticating module for the API.
 """
 import re
+from flask import abort, request
 from typing import List, TypeVar
-from flask import request
+from models.user import User
 
 
 class Auth:
     """Authentication class.
     """
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """confirms if the path requires authentication.
-        """
-        if path is not None and excluded_paths is not None:
-            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
-                pattern = ''
-                if exclusion_path[-1] == '*':
-                    pattern = '{}.*'.format(exclusion_path[0:-1])
-                elif exclusion_path[-1] == '/':
-                    pattern = '{}/*'.format(exclusion_path[0:-1])
-                else:
-                    pattern = '{}/*'.format(exclusion_path)
-                if re.match(pattern, path):
-                    return False
+   
+    def require_auth(
+        self, path: str, excluded_paths: List[str]
+    ) -> bool:
+        """ determines if authentication is required """
+        if path is None or not excluded_paths:
+            return True
+
+        if path[-1] != '/':
+            path = path + '/'
+        for pth in excluded_paths:
+            if pth[-1] == '*':
+                pth = pth[:-1] + '.*'
+            if re.fullmatch(pth, path):
+                return False
+
         return True
 
-    def authorization_header(self, request=None) -> str:
-        """Gets the authorization.
-        """
-        if request is not None:
-            return request.headers.get('Authorization', None)
+   def authorization_header(self, request=None) -> str:
+        """ doc str """
+        if request is None:
+            return None
+
+        auth_header = request.headers.get('Authorization')
+        if auth_header:
+            return auth_header
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
