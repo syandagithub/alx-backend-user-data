18980e8e17dd4c96751c9f46ed4da61d4e2c91ac
SYANDA MNTAMBO's GITHUB <112751864+syandagithub@users.noreply.github.com>
2024-07-17 19:13:03 +0200

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""DB module."""
+"""DB module.
+"""
 from sqlalchemy import create_engine, tuple_
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
@@ -11,7 +12,8 @@ from user import Base, User
 
 
 class DB:
-    """DB class."""
+    """DB class.
+    """
 
     def __init__(self) -> None:
         """Initialize a new DB instance.
@@ -23,14 +25,16 @@ class DB:
 
     @property
     def _session(self) -> Session:
-       
+        """Memoized session object.
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-       
+        """Adds a new user to the database.
+        """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
@@ -58,7 +62,8 @@ class DB:
         return result
 
     def update_user(self, user_id: int, **kwargs) -> None:
-   
+        """Updates a user based on a given id.
+        """
         user = self.find_user_by(id=user_id)
         if user is None:
             return
